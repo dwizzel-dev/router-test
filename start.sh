0b68fe746a384fba2b5f0462b71b47334dcd007a
dwizzel-dev <dwizzel.dev@gmail.com>
2018-02-15 17:31:10 -0500

    ssh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+vagrant="C:/Users/Dwizzel/Desktop/dwizzel.dev/virtualbox/VMs/centos/7"
+if ! [ -f "$vagrant/Vagrantfile" ]; then 
+    echo "Not a valid vagrant path: $vagrant"
+    exit 0
+else
+    vm=$(grep "config.vm.box" -m1 "$vagrant/Vagrantfile" | sed 's/.*"\([a-z].*\)"/\1/')
+    echo "Starting $vm"
+fi
+cd $vagrant
+echo "Vagrant up"
+vagrant up
+echo "Vagrant ssh"
+vagrant ssh
+echo "Vagrant halt"
+vagrant halt
+exit 0
